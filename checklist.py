485932400fe8d3da0f9cf7ed23e676c7787cb396
capt-aliengit config --global user.email eric.botcher@presidio.edu <ericbotcher@Aliens-MacBook-Air.local>
2018-09-12 18:49:21 -0700

    first commit

---
+++ checklist.py
@@ -0,0 +1,69 @@
+checklist = list()
+
+def create(item):
+    checklist.append(item)
+
+def read(index):
+    return checklist[index]
+
+def update(index, item):
+    checklist[index] = item
+
+def destroy(index):
+    checklist.pop(index)
+
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(str(index) + list_item)
+        index += 1
+
+def mark_completed(index):
+    checklist[int(index)].append("√")
+
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
+
+def select(function_code):
+    if function_code == "C":
+    elif function_code == "R":
+    elif function_code == "P":
+    elif function_code == "Q":
+        return False
+    else:
+        print("Unknown Option")
+    return True
+
+def test():
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+
+    destroy(1)
+
+    print(read(0))
+
+    list_all_items()
+    select("C")
+    # View the results
+    list_all_items()
+    # Call function with new value
+    select("R")
+    # View results
+    list_all_items()
+    # Continue until all code is run
+
+test()
+
+running = True
+while running:
+    selection = user_input(
+        "Press C to add to list, R to Read from list, P to display list, and Q to quit")
+    running = select(selection)
